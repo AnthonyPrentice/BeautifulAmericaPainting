22643d38f6092e0f0a69b83e3c3a883f66b577bc
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-06-18 12:23:38 -0700

    Fixed scaling issues in mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,18 @@ import Logo from "../img/Logo.png"
 
 function NavBar(){
     return (
-        <div className="navbar">
-            <img src={Logo} className="logo" width="150" height="75"/>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/Services">Services</NavLink>
-            <NavLink to="/About">About</NavLink>
-            <NavLink to="/Gallery">Gallery</NavLink>
-            <NavLink to="/Estimate">Estimate</NavLink>
+        <div className="navbar-wrapper">
+            <div className="navbar">
+                <img src={Logo} className="logo" width="150" height="75"/>
+                <NavLink className="navbar-links" to="/">Home</NavLink>
+                <NavLink className="navbar-links" to="/Services">Services</NavLink>
+                <NavLink className="navbar-links" to="/About">About</NavLink>
+                <NavLink className="navbar-links" to="/Gallery">Gallery</NavLink>
+                <NavLink className="navbar-links" to="/Estimate">Estimate</NavLink>
+            </div>
+            <div className="burger-menu">
+                
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MetaTags from 'react-meta-tags';
 import App from './App';
 import "./styles/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MetaTags>
+    {/*<meta name="viewport" content="width=1052" />*/}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    </MetaTags>
     <App />
   </React.StrictMode>
 );
