119a847e3f8879b7ff6e235ddbf41e04f6d8a314
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-06-22 14:23:54 -0700

    Finished NavBar with drop-down menu for mobile.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom"
+import { IconContext } from "react-icons";
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 import "../styles/NavBar.css"
 import Logo from "../img/Logo.png"
 
-function NavBar(){
+function NavBar() {
+    //dynamic components: bars-icon, exit-icon, bars-menu
+    const [BarsMenu, setBarsMenu] = useState(() => {
+        return {
+            barsIcon: "bars-icon",
+            exitIcon: "hidden",
+            barsMenu: "hidden"
+        };
+    });
+
+    function BarsMenuClicked() {
+        if(BarsMenu.barsMenu == "hidden") {
+            setBarsMenu(prevBarsMenu => ({
+                ...prevBarsMenu,
+                barsIcon: "hidden",
+                exitIcon: "exit-icon",
+                barsMenu: "bars-menu"
+            }));
+        } else{
+            setBarsMenu(prevBarsMenu => ({
+                ...prevBarsMenu,
+                barsIcon: "bars-icon",
+                exitIcon: "hidden",
+                barsMenu: "hidden"
+            }));
+        }
+    }
+
     return (
         <div className="navbar-wrapper">
             <div className="navbar">
@@ -14,9 +44,17 @@ function NavBar(){
                 <NavLink className="navbar-links" to="/About">About</NavLink>
                 <NavLink className="navbar-links" to="/Gallery">Gallery</NavLink>
                 <NavLink className="navbar-links" to="/Estimate">Estimate</NavLink>
+                <IconContext.Provider value={{color: "white", size: "2em"}}>
+                    <button className={BarsMenu.barsIcon} onClick={BarsMenuClicked}><FiMenu /></button>
+                    <button className={BarsMenu.exitIcon} onClick={BarsMenuClicked}><FiX /></button> 
+                </IconContext.Provider>
             </div>
-            <div className="burger-menu">
-                
+            <div className={BarsMenu.barsMenu}>
+                <NavLink className="bars-links" to="/">Home</NavLink>
+                <NavLink className="bars-links" to="/Services">Services</NavLink>
+                <NavLink className="bars-links" to="/About">About</NavLink>
+                <NavLink className="bars-links" to="/Gallery">Gallery</NavLink>
+                <NavLink className="bars-links" to="/Estimate">Estimate</NavLink>                
             </div>
         </div>
     );
