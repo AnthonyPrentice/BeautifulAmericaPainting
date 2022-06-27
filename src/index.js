df4a2dd20be2fae1b40cec1945ad30d90ccbecfd
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-06-27 12:23:42 -0700

    Changed fonts.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ root.render(
     <MetaTags>
     {/*<meta name="viewport" content="width=1052" />*/}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet" />
     </MetaTags>
     <App />
   </React.StrictMode>
