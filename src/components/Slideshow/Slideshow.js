e976612f3038628cc6a05076a805d7c6670462f1
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-06-30 10:25:03 -0700

    Updated notes, made progress on slideshow component

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,23 +1,25 @@
 import React, { useState }from 'react'
+import {} from 'react-transition-group'
 import Images from './SlideshowData'
 import '../../styles/Slideshow.css'
 
 function Slideshow(){
-    //state of current img
-    //button to switch between img
-    //perhaps a timer to automatically swtich between img
-    /**
-    return (
-        <div className='slideshow'>
-            {Images.map((img) => (
-                <img key={img} src={img} className='slideshow-img' />
-            ))}
-        </div>
-    );
-    **/
+    //current image
+    const [curImg, setCurImg] = useState(0) 
+    
+    function SlideshowNext() {
+        setCurImg((curImg+1)%3)
+    }
+
+    function SlideshowPrev() {
+        setCurImg((Images.length-((curImg-1)*-1))%Images.length)
+    }
+
     return (
         <span className='slideshow'>
-                <img key={Images[0]} src={Images[0]} className='slideshow-img' />
+            <img key={Images[curImg]} src={Images[curImg]} className='slideshow-img' />
+            <button className='btn-next' onClick={SlideshowNext}></button>
+            <button className='btn-prev' onClick={SlideshowPrev}></button>
         </span>
     );
 }
--- src/components/Slideshow/SlideshowData.js
+++ src/components/Slideshow/SlideshowData.js
@@ -1,6 +1,6 @@
-import img1 from '../../img/test/stock-1.jpeg'
-import img2 from '../../img/test/stock-2.jpeg'
-import img3 from '../../img/test/stock-3.jpeg'
+import img1 from '../../img/slideshow/stock-1-resized.jpg'
+import img2 from '../../img/slideshow/stock-2-resized.jpg'
+import img3 from '../../img/slideshow/stock-3-resized.jpg'
 
 const Images = [
     img1,
