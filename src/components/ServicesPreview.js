1302bcc82ab178f3772915e04be1f152afe4bba2
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-07-04 20:13:18 -0700

    Updated logo, almost done with ServicePreview section.

--- src/components/ServicesPreview.js
+++ src/components/ServicesPreview.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 import './css/ServicesPreview.css'
+import HouseImg from './img/ServicesPreview/pexels-binyamin-mellish-resized.png'
+import BuildingImg from './img/ServicesPreview/pexels-pixabay-resized.png'
 
 function ServicesPreview(){
     return (
             <div className='services-preview-wrapper'>
                 <div className='services-preview-item'>
                     <div className='services-preview-img-wrapper'>
-
+                        <img src={HouseImg} className='services-preview-img' />
                     </div>
                 </div>
                 <div className='services-preview-item'>
                     <div className='services-preview-img-wrapper'>
-
+                        <img src={BuildingImg} className='services-preview-img' />
                     </div>
                 </div>
             </div>
